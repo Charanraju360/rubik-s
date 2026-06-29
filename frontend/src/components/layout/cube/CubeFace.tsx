e5f7870ca2fa0e78993ec494e04916132b741e25
Charanraju360 <charanrajuk333@outlook.com>
2026-06-29 16:07:25 +0530

    feat: render 3x3 sticker grid in CubeFace

--- frontend/src/components/layout/cube/CubeFace.tsx
+++ frontend/src/components/layout/cube/CubeFace.tsx
@@ -2,7 +2,7 @@ import CubeSticker from "./CubeSticker";
 import { ThreeEvent } from "@react-three/fiber";
 
 interface FaceProps {
-  colors: string[];
+  colors: string[];        // 9 colors, top-left to bottom-right
   position: [number, number, number];
   rotation: [number, number, number];
   onStickerClick?: (index: number, e: ThreeEvent<MouseEvent>) => void;
@@ -14,5 +14,22 @@ export default function CubeFace({
   rotation,
   onStickerClick,
 }: FaceProps) {
-  return <group position={position} rotation={rotation}></group>;
+  const offsets: [number, number][] = [
+    [-1, 1],  [0, 1],  [1, 1],
+    [-1, 0],  [0, 0],  [1, 0],
+    [-1, -1], [0, -1], [1, -1],
+  ];
+
+  return (
+    <group position={position} rotation={rotation}>
+      {offsets.map(([x, y], i) => (
+        <CubeSticker
+          key={i}
+          position={[x, y, 0]}
+          color={colors[i] ?? "X"}
+          onClick={(e) => onStickerClick?.(i, e)}
+        />
+      ))}
+    </group>
+  );
 }
